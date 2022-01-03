7a0f1edaf521adeef4779e491d18afa5deb1fcd1
Vince <vincivenv@gmail.com>
2022-01-03 17:32:11 +0300

    fix: Get passowrd, hash and save

--- common/serializers.py
+++ common/serializers.py
@@ -10,4 +10,9 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
     
     def create(self, validated_data):
-        return super().create(validated_data)
+        password = validated_data.pop("password")
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
