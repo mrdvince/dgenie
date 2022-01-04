28a30cad67706944217a1809e9761ff315af4c95
Vince <vincivenv@gmail.com>
2022-01-04 13:12:02 +0300

    feat: Admin only views

--- dgenie/urls.py
+++ dgenie/urls.py
@@ -8,5 +8,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/admin/", include("umlawuli.urls")),
+    path("api/", include("umlawuli.urls")),
 ]
